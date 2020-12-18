383a2141ceab99ffd4ac581748b82a6d784c249d
osirisfrik <osiris@writecode.studio>
2020-12-17 20:42:38 -0800

    fix get element

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 function earnPoints() {
+  if (localStorage.getItem('debug-timeouts')) console.log('log earn', (new Date()).toString())
+  
   /**
    * @type { HTMLButtonElement }
    */
-  const btn = document.querySelector('button.tw-button.tw-button--success.tw-interactive')
+  const btn = document.querySelector('button.tw-button.tw-button--success')
 
   if (btn) btn.click()
+  if (btn && localStorage.getItem('log-earn')) console.log('clicked', (new Date()).toString())
 
-  setTimeout(earnPoints, 1 * 60 * 1000);
+  setTimeout(earnPoints, 1 * 60 * 1000)
 }
 
-setTimeout(earnPoints, 3000);
+setTimeout(earnPoints, 3000)
