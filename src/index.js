d10c239243207921713d705f742aa7c6c1c5c47f
osirisfrik <osiris.lpz96@gmail.com>
2023-10-12 11:10:03 -0700

    update query selector

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ function earnPoints() {
   /**
    * @type { HTMLButtonElement }
    */
-  const btn = document.querySelector('button.ScCoreButtonSuccess-sc-1qn4ixc-5')
+  const btn = document.querySelector('button.[aria-label="Claim Bonus"]')
 
   if (btn) btn.click()
   if (btn && localStorage.getItem('log-earn')) console.log('clicked', (new Date()).toString())
+  if (!btn && localStorage.getItem('log-earn')) console.log('btn not found', (new Date()).toString())
 
   setTimeout(earnPoints, 1 * 60 * 1000)
 }
